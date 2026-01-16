d28f2150ad851216bb577087e9095be84830feb2
Sanjay <116804212+skstlenovo@users.noreply.github.com>
2026-01-16 08:08:45 +0530

    Time: 7 ms (35.25%) | Memory: 46.7 MB (16.86%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,29 +1,42 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int maxLength= 0;
-        int l = 0;
-        int r = 1;
-        // if(s.isBlank()){
+        // int maxLength= 0;
+        // int l = 0;
+        // int r = 1;
+        // if(s.length()==1){
         //     return 1;
         // }
-        if(s.length()==1){
-            return 1;
-        }
-        while(l<s.length()-1){
-            List<Character> subString =new ArrayList<>();
-            char c = s.charAt(l);
-            subString.add(c);
-            r+= l;
-            while(r<s.length() && c!=s.charAt(r) && !subString.contains(s.charAt(r))){
-                subString.add(s.charAt(r));
+        // while(l<s.length()-1){
+        //     List<Character> subString =new ArrayList<>();
+        //     char c = s.charAt(l);
+        //     subString.add(c);
+        //     r+= l;
+        //     while(r<s.length() && c!=s.charAt(r) && !subString.contains(s.charAt(r))){
+        //         subString.add(s.charAt(r));
+        //         r++;
+        //     }
+        //     l++;
+        //     r=1;
+        //     maxLength = Math.max(maxLength,subString.size());
+        // }
+        // return maxLength;  
+
+        Set<Character> set = new HashSet<>();
+        int l = 0, r = 0, maxLength = 0;
+
+        while (r < s.length()) {
+            char current = s.charAt(r);
+
+            if (!set.contains(current)) {
+                set.add(current);
+                maxLength = Math.max(maxLength, r - l + 1);
                 r++;
+            } else {
+                set.remove(s.charAt(l));
+                l++;
             }
-            l++;
-            r=1;
-            maxLength = Math.max(maxLength,subString.size());
         }
 
-        return maxLength;
-        
+        return maxLength; 
     }
 }
